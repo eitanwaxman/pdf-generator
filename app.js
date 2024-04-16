b13394d2df0c22917b8c157c2caf4bc6cc4d9d10
Eitan <eitanwaxman@gmail.com>
2024-04-16 13:22:14 +0530

    remove logs, add href to watermark

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/', async (req, res) => {
     console.log(req.body);
     const { url, options } = req.body;
-    console.log("url", url)
     try {
         const PDF = await exportWebsiteAsPdf(url, options);
         console.log("PDF", PDF)
@@ -45,23 +44,25 @@ async function exportWebsiteAsPdf(websiteUrl, options) {
 
     await page.emulateMediaType('screen');
 
-    page.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-            console.log(`${i}: ${msg.args()[i]}`);
-    });
-
     if (free) {
         await page.evaluate(() => {
             const wixAds = document.getElementById('WIX_ADS');
             wixAds.remove();
             const uppermostElement = document.body.children[0];
             const watermark = document.createElement('div');
-            watermark.innerHTML = "Generated using PDF Generator App by The Wix Wiz. Visit thewixwiz.com/wix-apps to learn more";
+
+            const watermarkLink = document.createElement('a');
+            watermarkLink.href = 'https://thewixwiz.com/wix-apps'; 
+            watermarkLink.textContent = "Generated using PDF Generator App by The Wix Wiz. Visit thewixwiz.com/wix-apps to learn more";
+            watermarkLink.style.color = 'inherit'; 
+            watermarkLink.style.textDecoration = 'none';
+            watermark.appendChild(watermarkLink);
+            
             watermark.style.width = '100%';
             watermark.style.textAlign = 'center';
             watermark.style.opacity = '0.7';
             watermark.style.marginTop = '20px';
-            watermark.style.fontSize = '12px';
+            watermark.style.fontSize = '14px';
             watermark.style.fontFamily = 'Arial';
             watermark.style.zIndex = '1000';
             document.body.insertBefore(watermark, uppermostElement);
